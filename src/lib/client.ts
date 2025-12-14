dca38727598fbd170c05689f44c1e4216d6a5cf7
Raj <rajfekar.143@gmail.com>
2025-12-15 01:22:44 +0530

    :udpate

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,4 +1,6 @@
 import { App } from "@/app/api/[[...slugs]]/route"
 import { treaty } from "@elysiajs/eden"
 
-export const client = treaty<App>(process.env.APP_URL || "").api
+export const client = treaty<App>(
+  `${process.env.APP_URL || "realtime-private-chat-puce.vercel.app"}`
+).api
